f1a9f5de25e66d47c7ae47a749652b936c0aa451
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-03-09 10:14:15 +0200

    adds telescope

---
+++ lua/telescope-config.lua
@@ -0,0 +1,5 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<Space>ff', builtin.find_files, {})
+vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<Space>fb', builtin.buffers, {})
+vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
